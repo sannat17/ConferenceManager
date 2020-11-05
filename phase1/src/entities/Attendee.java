667117c0daa504c7ac367f817794d6f8d441beac
Marko Bachvarovski <marko.bachvarovski@mail.utoronto.ca>
2020-11-05 17:24:29 -0500

    Javadocs added to Attendee, Organizer and Speaker. - Marko

--- phase1/src/entities/Attendee.java
+++ phase1/src/entities/Attendee.java
@@ -1,7 +1,12 @@
 package entities;
 
 public class Attendee extends User{
-
+    /**
+     * Initializes an Attendee by calling the super constructor from User
+     * @param username
+     * @param password
+     * @param ID
+     */
     public Attendee(String username, String password, int ID) {
         super(username, password, ID);
     }
--- phase1/src/entities/Speaker.java
+++ phase1/src/entities/Speaker.java
@@ -2,23 +2,28 @@ package entities;
 
 import java.util.ArrayList;
 
-public class Speaker extends User{
-
-    int numberOfEvents;
+public class Speaker extends Organizer{
+    /**
+     * Has the list of IDs of events that the Speaker is speaking at
+     */
     ArrayList listOfEventIDs;
 
+    /**
+     * Calls the super constructor from Organizer
+     * @param username
+     * @param password
+     * @param ID
+     */
     public Speaker(String username, String password, int ID) {
         super(username, password, ID);
-        numberOfEvents = 0;
     }
 
+    /**
+     * Returns the number of events this Speaker is speaking at
+     * @return the size of the list of event IDs
+     */
     public int getNumberOfEvents(){
-        return this.numberOfEvents;
-    }
-
-    public void addTalk(Event newEvent){
-        listOfEventIDs.add(newEvent);
-        numberOfEvents++;
+        return this.listOfEventIDs.size();
     }
 
 }
