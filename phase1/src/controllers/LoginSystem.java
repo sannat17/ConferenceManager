665add1163f3d56730e4fa6b51d2d705c94aeea0
Shiven Taneja <shiven.taneja@gmail.com>
2020-11-09 22:26:38 +0400

    Changed UserManager and LoginSystem (mainly for SOLID and added new features)

--- phase1/src/controllers/LoginSystem.java
+++ phase1/src/controllers/LoginSystem.java
@@ -1,14 +1,26 @@
 package controllers;
 import java.util.*;
+import entities.*;
+import useCases.UserManager;
 
 public class LoginSystem {
     public static String[] getLoginInformation(){ //retreives username and password from a user
         Scanner s = new Scanner(System.in);
         System.out.println("Please enter your Username:");
-        String userName = s.nextLine();
+        String username = s.nextLine();
         System.out.println("Please enter your Password:");
         String password = s.nextLine();
-        String[] userPass = new String[]{userName, password};
-        return userPass;
+        return new String[]{username, password};
+    }
+
+    public static boolean login(){
+        String[] loginDetails = getLoginInformation();
+        ArrayList<User> users = UserManager.getAllUsers();
+        for (User u: users){
+            if ((loginDetails[0].equals(u.getUsername())) && (loginDetails[1].equals(u.getPassword()))){
+                return true;
+            }
+        }
+        return false;
     }
 }
