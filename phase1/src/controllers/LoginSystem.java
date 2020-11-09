108837aebf0beabb773818c9fcbbd55203956a03
Roger Lam <roger.l.jiahao2@gmail.com>
2020-11-09 17:17:58 -0500

    Fix JavaDoc

--- phase1/src/controllers/LoginSystem.java
+++ phase1/src/controllers/LoginSystem.java
@@ -1,8 +1,8 @@
 package controllers;
-import java.util.*;
+import java.util.Scanner;
 
 public class LoginSystem {
-    public static String[] getLoginInformation(){ //retreives username and password from a user
+    public static String[] getLoginInformation(){ //retrieves username and password from a user
         Scanner s = new Scanner(System.in);
         System.out.println("Please enter your Username:");
         String userName = s.nextLine();
