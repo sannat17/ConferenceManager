942b003171b1dfaa1113230ab75e4946e2d47341
sannat17 <sannat1701@gmail.com>
2020-11-11 03:59:43 +0530

    Improved consistency in message creation by updating Message and MessageManager

--- phase1/src/entities/Message.java
+++ phase1/src/entities/Message.java
@@ -11,17 +11,7 @@ public class Message {
     private int receiverID;
     private LocalTime timeSent;
     private int messageID;
-    private int replyToID;  //stores the ID of the message that this is a reply to(if it's a reply)
-
-
-    public Message(int senderID,  int receiverID, int messageID, String messageContent){
-        this.messageID = messageID;
-        this.senderID = senderID;
-        this.receiverID = receiverID;
-        this.messageContent = messageContent;
-        this.replyToID = -1;
-        this.timeSent = LocalTime.now();  //immutable object that represents a time without a date and a time-zone
-    }
+    private int replyToID;  //stores the ID of the message that this is a reply to (if it's a reply); -1 if not a reply
 
     public Message(int senderID,  int receiverID, int messageID, int replyToID, String messageContent){
         this.messageID = messageID;
