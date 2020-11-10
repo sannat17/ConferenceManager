41d95c32aed774d8e64f626eb3e8c2a17a2cb3ac
sannat17 <sannat1701@gmail.com>
2020-11-11 04:06:15 +0530

    Added javadoc to clarify difference in similar makeMessage methods in MessageManager

--- phase1/src/useCases/MessageManager.java
+++ phase1/src/useCases/MessageManager.java
@@ -12,6 +12,16 @@ public class MessageManager {
         return messageHashMap.get(ID);
     }
 
+    /** Make an instance of a message with a specified ID (eg. while loading from a file) and store it in messageHashMap
+     *  to be indexed by its ID.
+     *
+     * @param senderID
+     * @param receiverID
+     * @param messageID
+     * @param replyToID
+     * @param messageContent
+     * @return
+     */
     public static boolean makeMessage(int senderID, int receiverID, int messageID, int replyToID, String messageContent) {
 
         if (messageHashMap.containsKey(messageID)) {return false;}
@@ -22,6 +32,15 @@ public class MessageManager {
         return true;
     }
 
+    /** Make an instance of a new message (eg. by a user) and automatically assign it a new unique ID. Store this in the
+     * messageHashMap to be indexed by this new ID.
+     *
+     * @param senderID
+     * @param receiverID
+     * @param replyToID
+     * @param messageContent
+     * @return
+     */
     public static boolean makeNewMessage(int senderID, int receiverID, int replyToID, String messageContent) {
 
         int messageID = Collections.max(messageHashMap.keySet()) + 1;
