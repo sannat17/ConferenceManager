2d0e07f9e6216ad8efa606a117423a3855868a50
Shiven Taneja <shiven.taneja@gmail.com>
2020-11-11 21:37:34 +0400

    Added readFiles in main and sign out in user

--- phase1/src/Main.java
+++ phase1/src/Main.java
@@ -6,6 +6,9 @@ import useCases.*;
 public class Main {
     public static void main(String[] args) {
         //need to get all users and messages from files
+        UserIO.readFile("/data.users.txt");
+        EventIO.readFile("/data.events.txt");
+        MessageIO.readFile("/data.messages.txt");
         User loggedInUser = LoginSystem.login();
     }
 }
