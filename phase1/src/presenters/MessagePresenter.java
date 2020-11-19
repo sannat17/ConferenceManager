9ba7f94372f523690375b25e5cfc3a4ddd3e328f
DivijSanjanwala <59158971+DivijSanjanwala@users.noreply.github.com>
2020-11-19 18:40:51 -0500

    MessagePresenter Implemented

--- phase1/src/presenters/MessagePresenter.java
+++ phase1/src/presenters/MessagePresenter.java
@@ -25,17 +25,28 @@ public class MessagePresenter {
         return formattedOptions.toString();
     }
 
+
     /**
      * Format and return content of all the messages received by this user
      * @param user the user whose messages you want to display
      * @return the formatted string with content of all the messages received by this user
      */
     public static String displayReceivedMessagesOfUser(User user){
+
+
         StringBuilder messagesOfUser = new StringBuilder();
+
         ArrayList<Message> allReceivedMessages = MessageManager.getAllReceivedMessages(user.getUserID());
         for (Message message : allReceivedMessages) {
-            messagesOfUser.append(message.getMessageContent()).append("\n");
+
+            if (message.getReplyToID() == -1) {
+
+                messagesOfUser.append(message.getMessageContent()).append("\n");
+            } else {
+                messagesOfUser.append(message.getMessageContent()).append("\n");
+            }
         }
+
         return messagesOfUser.toString();
     }
 
@@ -47,20 +58,21 @@ public class MessagePresenter {
         Map<Integer, ArrayList<String>> presenterHashMap = new HashMap<>();
 
 
-//        for (Integer integer: MessageController.presenterMessageHashMap.keySet()) {
-//
-//            for (Message message: MessageManager.getAllSentMessages(integer)) {
-//
-////              This appends all the strings of messages into a list
-//                messages.add(message.getMessageContent());
-//                presenterHashMap.put(integer, messages);
-//            }
-//        }
+        for (Integer integer: MessageController.presenterMessageHashMap.keySet()) {
+
+            for (Message message: MessageManager.getAllSentMessages(integer)) {
+
+//              This appends all the strings of messages into a list
+                messages.add(message.getMessageContent());
+                presenterHashMap.put(integer, messages);
+            }
+        }
 
         for (Integer userID: presenterHashMap.keySet()) {
 
             String finalMessage = "These are all the messages sent by the user with ID"
                     + userID + formatOptions(presenterHashMap.get(userID));
+
         }
         return finalMessage;
     }
