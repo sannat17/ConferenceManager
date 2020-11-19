befa89d9cec1d8d19ed2ef24af82bf97d7707f53
Marko Bachvarovski <marko.bachvarovski@mail.utoronto.ca>
2020-11-19 17:24:58 -0500

    MessageController fixed, javadocs added- Marko

--- phase1/src/controllers/MessageController.java
+++ phase1/src/controllers/MessageController.java
@@ -1,6 +1,6 @@
 package controllers;
 
-import entities.Event;
+//import entities.Event;
 import entities.Message;
 
 import java.util.ArrayList;
@@ -13,37 +13,29 @@ import static useCases.MessageManager.makeNewMessage;
 /** A message controller which handles user input for message content */
 public class MessageController {
 
-    public static HashMap<Integer, ArrayList<Message>> presenterMessageHashMap;
+    //public static HashMap<Integer, ArrayList<Message>> presenterMessageHashMap;
 
-    public static void replyOrNewMessage(){
-        Scanner s = new Scanner(System.in);
-        System.out.println("Would you like to reply to a message or start a new one?(Type true if reply)");
-        boolean reply = s.nextBoolean(); //decides whether sender is replying or starting new message
+    /**
+     * Call this method anytime a message needs to be created
+    the method determines whether the message is new or a reply and creates a
+    Message object accordingly
+    @param reply - determines whether the message created is a reply or not
+    @param receiverID - the ID of the receiver
+    @param senderID - the ID of the sender
+    @param replyToID - the ID of the message to which we are replying (if applicable)
+    @param content - the content of the message
+    */
+
+    public static void replyOrNewMessage(boolean reply, int receiverID, int senderID,
+                                         int replyToID, String content){
         if (reply){
-            System.out.println("Who are you replying to?");
-            int receiverID = s.nextInt(); //gets id of the receiver
-            Message messageRepliedTo = presenterMessageHashMap.get(receiverID).
-                    get(presenterMessageHashMap.get(receiverID).size() - 1);
-            //gets the last message the receiver has sent
-            String content = getContent();
-            makeNewMessage(messageRepliedTo.getReceiverID(), receiverID,
-                    messageRepliedTo.getMessageID(), content);
+            makeNewMessage(senderID, receiverID, replyToID, content);
+            //make a message with the given input
         }
         else{
-            System.out.println("Enter your ID:");
-            int senderID = s.nextInt();
-            System.out.println("Who are you sending a message to?");
-            int receiverID = s.nextInt();
-            String content = getContent();
-            makeNewMessage(senderID, receiverID, -1, content); //-1 because it isn't a reply
-
+            makeNewMessage(senderID, receiverID, -1, content);
+            //-1 because it isn't a reply
         }
     }
-    public static String getContent(){
-        Scanner s = new Scanner(System.in);
-        System.out.println("Please enter your message:");
-        return s.nextLine();
-    }
-
 
 }
