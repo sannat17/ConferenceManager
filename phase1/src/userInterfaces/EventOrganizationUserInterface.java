944457b2d6181c5f1b718b632564d5ff9eed6347
HaydenWolff <45108375+HaydenWolff@users.noreply.github.com>
2020-11-20 15:42:34 -0500

    Changes to EventOrganizationUserInterface.java

--- phase1/src/userInterfaces/EventOrganizationUserInterface.java
+++ phase1/src/userInterfaces/EventOrganizationUserInterface.java
@@ -18,29 +18,45 @@ public class EventOrganizationUserInterface {
      */
     public static void loadEventOrganization(User user) {
         LocalDateTime eventDate = null;
+        boolean keepGoing = false;
+        int roomNumber = -1;
+        //int speakerID = -1;
+        //int organizerID = -1;
+
         Scanner u = new Scanner(System.in);
         System.out.println("Please enter the title of the event:");
         String title = u.nextLine();
         System.out.println("Please enter the year,month and date and time of the event (dd-MMM-yyyy: HH:mm:ss): \n" +
                 "For Example, November 20th 2020 at 6:35:05PM will be written as 20-Nov-2020 18:35:05");
-        String dateTime = u.nextLine();
 
-        try {
-            DateTimeFormatter formatter= DateTimeFormatter.ofPattern("dd-MMM-yyyy HH:mm:ss");
-            eventDate = LocalDateTime.parse(dateTime, formatter);
-        } catch (DateTimeParseException e) {
-            System.out.println("Please enter a valid format");
-            //loadEventOrganization(user);
+        while (!keepGoing) {
+            try {
+                String dateTime = u.nextLine();
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMM-yyyy HH:mm:ss");
+                eventDate = LocalDateTime.parse(dateTime, formatter);
+                keepGoing = true;
+            } catch (DateTimeParseException e) {
+                System.out.println("Please enter a valid format, For Example, November 20th 2020 at 6:35:05PM will be written as 20-Nov-2020 18:35:05:");
+            }
         }
-
+        keepGoing = false;
         System.out.println("Please enter the room number of the event:");
-        String roomNumber = u.nextLine();
-        System.out.println("Please enter the ID of the speaker of the event:");
+        while (!keepGoing){
+            try {
+                String roomNumberString = u.nextLine();
+                roomNumber = Integer.parseInt(roomNumberString);
+                keepGoing = true;
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a valid room number:");
+            }
+        }
+        //keepGoing = false;
+        System.out.println("Please enter the username of the speaker of the event:");
         String speakerID = u.nextLine();
-        System.out.println("Please enter the ID of the organizer of the event:");
+        System.out.println("Please enter the username of the organizer of the event:");
         String organizerID = u.nextLine();
 
-        EventManager.makeNewEvent(title, eventDate, Integer.parseInt(roomNumber), Integer.parseInt(speakerID), Integer.parseInt(organizerID));
+        EventManager.makeNewEvent(title, eventDate, roomNumber, Integer.parseInt(speakerID), Integer.parseInt(organizerID));
     }
 
 }
