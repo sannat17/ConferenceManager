45f785646138d06744fab393e61677d98a040a23
Wayne Zhu <way87767@gmail.com>
2020-11-19 23:18:18 -0500

    added javadocs to gateways

--- phase1/src/gateways/UserIO.java
+++ phase1/src/gateways/UserIO.java
@@ -14,9 +14,11 @@ public class UserIO {
 
     private static final String delimiter = Character.toString((char) 31);
 
+    /**
+     * reads in user file and passes user data to UserManager
+     * @param dir - the file path relative to the project root
+     */
     public static void readFile(String dir){
-        //reads in user file and returns HashMap mapping username to password
-
         try{
             File file = new File(dir);
             Scanner fs = new Scanner(file);
@@ -38,8 +40,11 @@ public class UserIO {
         }
     }
 
+    /**
+     * writes all user data from UserManager to file
+     * @param dir - the file path relative to the project root
+     */
     public static void writeFile(String dir){
-        //writes all user data from UserManager to file
         try{
             File file = new File(dir);
             boolean existing = file.createNewFile(); //attempt to create new file if not preexisting
