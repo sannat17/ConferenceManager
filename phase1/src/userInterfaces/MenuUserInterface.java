24477a510e38c7aa047101c5b27a770bd2e1cdb0
HaydenWolff <45108375+HaydenWolff@users.noreply.github.com>
2020-11-20 11:49:29 -0500

    Minor Fix

--- phase1/src/userInterfaces/MenuUserInterface.java
+++ phase1/src/userInterfaces/MenuUserInterface.java
@@ -23,7 +23,7 @@ public class MenuUserInterface {
             System.out.println("Enter menu option:");
 
             String menuChoice = s.nextLine();
-            System.out.println(user.getMenuOptions());
+
             if (user.getMenuOptions().contains(menuChoice)) {
                 switch (menuChoice) {
                     case "messages":
