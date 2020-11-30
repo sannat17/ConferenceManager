bbe89235cae8a69144217ed8957f68c771c35933
DivijSanjanwala <59158971+DivijSanjanwala@users.noreply.github.com>
2020-11-30 05:09:27 -0500

    Messagemanager getters implemented

--- phase2/src/useCases/MessageManager.java
+++ phase2/src/useCases/MessageManager.java
@@ -13,52 +13,57 @@ public class MessageManager {
         return messageHashMap.get(ID);
     }
 
-    /** Make an instance of a message with a specified ID (eg. while loading from a file) and store it in messageHashMap
-     *  to be indexed by its ID.
+    /**
+     * Make an instance of a message with a specified ID (eg. while loading from a file) and store it in messageHashMap
+     * to be indexed by its ID.
      *
-     * @param senderID The ID of the sender of the message
-     * @param receiverID The ID of the receiver of the message
-     * @param messageID The ID of the message
-     * @param replyToID The ID of the reply of the message
+     * @param senderID       The ID of the sender of the message
+     * @param receiverID     The ID of the receiver of the message
+     * @param messageID      The ID of the message
+     * @param replyToID      The ID of the reply of the message
      * @param messageContent The ID of the sender of the message
      * @return returns the boolean true is the message is made!
      */
-    public static boolean makeMessage(int senderID, int receiverID, int messageID, int replyToID, String messageContent) {
+    public static boolean makeMessage(int senderID, int statusID, int receiverID, int messageID, int replyToID, String messageContent) {
 
-        if (messageHashMap.containsKey(messageID)) {return false;}
+        if (messageHashMap.containsKey(messageID)) {
+            return false;
+        }
 
-        Message m = new Message(senderID, receiverID, messageID, replyToID, messageContent);
+        Message m = new Message(senderID, statusID, receiverID, messageID, replyToID, messageContent);
 
         messageHashMap.put(messageID, m);
         return true;
     }
 
-    /** Make an instance of a new message (eg. by a user) and automatically assign it a new unique ID. Store this in the
+    /**
+     * Make an instance of a new message (eg. by a user) and automatically assign it a new unique ID. Store this in the
      * messageHashMap to be indexed by this new ID.
      *
-     * @param senderID The ID of the sender of the message
-     * @param receiverID The ID of the receiver of the message
-     * @param replyToID The ID of the reply of the message
+     * @param senderID       The ID of the sender of the message
+     * @param receiverID     The ID of the receiver of the message
+     * @param replyToID      The ID of the reply of the message
      * @param messageContent The ID of the sender of the message
      * @return returns true if a new message is made.
      */
-    public static boolean makeNewMessage(int senderID, int receiverID, int replyToID, String messageContent) {
+    public static boolean makeNewMessage(int senderID, int statusID, int receiverID, int replyToID, String messageContent) {
 
         int messageID;
 
-        if (messageHashMap.keySet().isEmpty()){
+        if (messageHashMap.keySet().isEmpty()) {
             messageID = 0;
-        } else{
+        } else {
             messageID = Collections.max(messageHashMap.keySet()) + 1;
         }
 
-        Message m = new Message(senderID, receiverID, messageID, replyToID, messageContent);
+        Message m = new Message(senderID, receiverID, messageID, replyToID, statusID, messageContent);
 
         messageHashMap.put(messageID, m);
         return true;
     }
 
-    /** Return all messages sent by user.
+    /**
+     * Return all messages sent by user.
      *
      * @param ID The userID of the user
      * @return return an ArrayList of all the Message objects of messages sent by the user
@@ -66,7 +71,7 @@ public class MessageManager {
     public static ArrayList<Message> getAllSentMessages(int ID) {
 
         ArrayList<Message> to_return = new ArrayList<>();
-        for (Message m: messageHashMap.values()) {
+        for (Message m : messageHashMap.values()) {
             if (m.getSenderID() == ID) {
                 to_return.add(m);
             }
@@ -74,7 +79,8 @@ public class MessageManager {
         return to_return;
     }
 
-    /** Return all messages received by user
+    /**
+     * Return all messages received by user
      *
      * @param ID The userID of the user
      * @return return an ArrayList of all the Message objects of messages received by the user
@@ -82,7 +88,7 @@ public class MessageManager {
     public static ArrayList<Message> getAllReceivedMessages(int ID) {
 
         ArrayList<Message> to_return = new ArrayList<>();
-        for (Message m: messageHashMap.values()) {
+        for (Message m : messageHashMap.values()) {
             if (m.getReceiverID() == ID) {
                 to_return.add(m);
             }
@@ -90,7 +96,8 @@ public class MessageManager {
         return to_return;
     }
 
-    /** Return all the messages in the system.
+    /**
+     * Return all the messages in the system.
      *
      * @return return an ArrayList of all Message objects that exist on the system.
      */
@@ -98,7 +105,8 @@ public class MessageManager {
         return (ArrayList<Message>) messageHashMap.values();
     }
 
-    /** Return all the information of the messages in the system
+    /**
+     * Return all the information of the messages in the system
      *
      * @return return an ArrayList of the string representation of all Message objects that exist in the system.s
      */
@@ -106,11 +114,10 @@ public class MessageManager {
 
         ArrayList<String> to_return = new ArrayList<>();
 
-        for (Message m: messageHashMap.values()) {
+        for (Message m : messageHashMap.values()) {
             to_return.add(m.toString());
         }
 
         return to_return;
     }
-
 }
