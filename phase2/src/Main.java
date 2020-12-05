21fa84a29de22a95e71feb39f58c760b4464ad9c
Wayne Zhu <way87767@gmail.com>
2020-12-04 19:21:00 -0500

    added dedicated auth manager, tracking current user

--- phase2/src/Main.java
+++ phase2/src/Main.java
@@ -1,8 +1,6 @@
-import controllers.*;
-import entities.*;
 import gateways.*;
-import useCases.*;
-import userInterfaces.MenuUserInterface;
+import userInterfaces.*;
+import useCases.AuthManager;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -13,20 +11,12 @@ public class Main {
         UserIO.readFile();
         EventIO.readFile();
         MessageIO.readFile();
+
         while (true) {
-            User loggedInUser = null;
-            Scanner s = new Scanner(System.in);
-            while (loggedInUser == null) {
-                System.out.println("Please enter your Username:");
-                String username = s.nextLine();
-                System.out.println("Please enter your Password:");
-                String password = s.nextLine();
-                loggedInUser = LoginSystem.loginCheck(username, password);
-                if (loggedInUser == null) {
-                    System.out.println("Your username or password is incorrect. Please enter them again.");
-                }
-            }
-            MenuUserInterface.loadMenu(loggedInUser);
+            LoginUserInterface.login();
+            MenuUserInterface.loadMenu(AuthManager.getLoggedInUser());
+            AuthManager.logout();
+
             UserIO.writeFile();
             MessageIO.writeFile();
             EventIO.writeFile();
---
+++ phase2/src/useCases/AuthManager.java
@@ -0,0 +1,32 @@
+package useCases;
+
+import entities.User;
+
+import java.util.ArrayList;
+
+public class AuthManager {
+
+    private static User loggedInUser = null;
+
+    public static boolean login(String username, String password) {
+        ArrayList<User> users = UserManager.getAllUsers();
+        if (users.isEmpty()) {
+            return false;
+        }
+        for (User u : users) {
+            if ((username.equals(u.getUsername())) && (password.equals(u.getPassword()))) {
+                loggedInUser = u;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void logout(){
+        loggedInUser = null;
+    }
+
+    public static User getLoggedInUser(){
+        return loggedInUser;
+    }
+}
