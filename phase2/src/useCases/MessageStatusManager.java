b8162fb10cbf0914b3f7c817c9e4ac03e79c8980
DivijSanjanwala <59158971+DivijSanjanwala@users.noreply.github.com>
2020-12-11 19:09:23 -0500

    Adding javadoc for the code implemented earlier.

--- phase2/src/useCases/MessageStatusManager.java
+++ phase2/src/useCases/MessageStatusManager.java
@@ -6,29 +6,55 @@ import java.util.ArrayList;
 
 public class MessageStatusManager {
 
-
+    /**
+     * This function gets the status of the message, the descriptors of the different statuses are given in the
+     * Message entity.
+     * @param MessageID The ID of the message
+     * @return returns the int of the status of the message, ie: the statusID by calling the getter getStatusID().
+     */
     public static int getStatusOfMessage(int MessageID){
         return MessageManager.getMessage(MessageID).getStatusID();
     }
 
+    /**
+     * This function marks the message as archived by setting the status of the Archived message as -2.
+     * @param MessageID The ID of the message.
+     */
     public static void markMessageAsArchived(int MessageID) {
 
         MessageManager.getMessage(MessageID).setStatusID(-2);
     }
 
+    /**
+     * This function marks the message as archived by setting the status of the Archived message as -2.
+     * @param MessageID The ID of the message.
+     */
     public static void markMessageAsUnArchived(int MessageID) {
 
         MessageManager.getMessage(MessageID).setStatusID(1);
     }
 
+    /**
+     * This function marks the message unread by setting the statusID of the message as 0.
+     * @param MessageID The ID of the message.
+     */
     public static void markMessageAsUnread(int MessageID) {
 
        MessageManager.getMessage(MessageID).setStatusID(0);
     }
 
+    /**
+     * This function marks the message as archived by setting the status of the Archived message as -2.
+     * @param MessageID The ID of the message.
+     */
     public static void markMessageAsRead(int MessageID) { MessageManager.getMessage(MessageID).setStatusID(1);
     }
 
+    /**
+     * This function gets all the received messages by the user according to the statusID.
+     * @param statusID The statusID of the message.
+     * @param userID The userID of the user.
+     */
     public static ArrayList<Message> getAllReceivedMessagesByStatus(int userID, int statusID) {
 
         ArrayList<Message> to_return = new ArrayList<>();
