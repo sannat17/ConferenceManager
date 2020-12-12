42b1477aff7597ea72c6a1da0922569563ab58c3
ananyajha <ananya.jha@mail.utoronto.ca>
2020-12-11 21:08:19 -0500

    Added javadocs to MessagePresenter

--- phase2/src/GUIPresenters/MessagePresenter.java
+++ phase2/src/GUIPresenters/MessagePresenter.java
@@ -11,7 +11,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MessagePresenter {
-
+    /**
+     * Performs actions based on the message option selected by the user
+     * @param messagePanelChoice the message option selected by the user
+     *
+     */
     public static void nextMessagePanel(String messagePanelChoice){
         System.out.println(messagePanelChoice);
         switch (messagePanelChoice){
@@ -42,19 +46,39 @@ public class MessagePresenter {
         }
     }
 
+    /**
+     * Adds the reply to the reply panel
+     * @param content content of the reply
+     * @param replyID replyID of the message that is to be replied to
+     * @param recipientName name of the message reciever
+     */
     public static void replyToMessage(String content, int replyID, String recipientName){
         mainView.toReplyPanel(content, replyID, recipientName);
     }
 
+    /**
+     * Makes a new reply to a message
+     * @param recieverID ID of the message reciever
+     * @param content content of the message reply
+     * @param replyID replyID of the message
+     */
     public static void reply(int recieverID, int replyID, String content){
         MessageManager.makeNewMessage(AuthManager.getLoggedInUserID(), recieverID, replyID, content);
     }
-
+    /**
+     * Makes a new message
+     * @param receiver username of the message reciever
+     * @param content content of the message
+     */
     public static void sendMessage(String receiver, String content) {
         MessageManager.makeNewMessage(AuthManager.getLoggedInUserID(),
                 UserManager.giveIDOfUsername(receiver), -1, content);
     }
-
+    /**
+     * Sends a message to multiple users of the same type
+     * @param type type of the user
+     * @param content content of the message
+     */
     public static void sendMessageToAllType(String type, String content){
         ArrayList<String> users = UserTypeManager.getUsersByType(type);
         for (String u: users){
@@ -62,6 +86,11 @@ public class MessagePresenter {
         }
     }
 
+    /**
+     * Sends a message to all attendees of a talk
+     * @param talkID ID of the talk
+     * @param content content of the message reply
+     */
     public static void sendMessageToAllAttendeesOfTalk(int talkID, String content){
         ArrayList<Integer> attending = EventManager.getAttendingSpecificEvent(talkID);
         for (int i: attending){
@@ -69,6 +98,9 @@ public class MessagePresenter {
         }
     }
 
+    /**
+     * Returns a hashmap of all users in the current users' inbox
+     */
     public static HashMap<Integer, String> getUsernamesFromInbox(){
         HashMap<Integer, String> userHash = new HashMap<>();
         ArrayList<Message> messages = MessageManager.getAllReceivedMessages(
@@ -85,6 +117,9 @@ public class MessagePresenter {
         return userHash;
     }
 
+    /**
+     * Returns a hashmap of all users in the current users' archived messages
+     */
     public static HashMap<Integer, String> getUsernamesFromArchived(){
         HashMap<Integer, String> userHash = new HashMap<>();
         ArrayList<Message> messages = MessageManager.getAllReceivedMessages(
@@ -101,18 +136,32 @@ public class MessagePresenter {
         return userHash;
     }
 
+    /**
+     * Returns the list of all talks by the speaker
+     */
     public static String[] getTalksBySpeaker(){
         return EventManager.getAllEventsBySpeaker(AuthManager.getLoggedInUserID()).toArray(new String[0]);
     }
 
+    /**
+     * Returns a list of all talks
+     */
     public static String[] getAllTalks(){
         return EventManager.getAllEventsTitles().toArray(new String[0]);
     }
 
+    /**
+     * Returns the ID of the talk using given title
+     * @param title Title of the talk
+     */
     public static int getIDOfTalkByTitle(String title){
         return EventManager.giveEventIDOfTitle(title);
     }
 
+    /**
+     * Returns the status of the message
+     * @param ID ID of the message whose status is returned
+     */
     public static String getMessageStatus(int ID){
         if(MessageStatusManager.getStatusOfMessage(ID) == 0) {
             return "Unread";
@@ -123,35 +172,70 @@ public class MessagePresenter {
         }
         return "";
     }
-
+    /**
+     * Deletes the message
+     * @param ID ID of the message that is to be deleted
+     */
     public static void deleteMessage(int ID){MessageController.deleteMessage(ID);}
-
+    /**
+     * Marks the message as Archived
+     * @param ID ID of the message that is to be marked
+     */
     public static void markArchived(int ID){MessageController.markAsArchived(ID);}
 
+    /**
+     * Marks the message as read
+     * @param ID ID of the message that is to be marked
+     */
     public static void markRead(int ID) {MessageStatusManager.markMessageAsRead(ID);}
 
+    /**
+     * Marks the message as unread
+     * @param ID ID of the message that is to be marked
+     */
     public static void markUnread(int ID){MessageStatusManager.markMessageAsUnread(ID);}
 
+    /**
+     * Returns the message content
+     * @param ID ID of the message whose contents are supposed to be returned
+     */
     public static String getMessageContent(int ID){return MessageManager.getContent(ID);}
 
+    /**
+     * Returns the message options for the user who is currently logged in
+     */
     public static String[] messageOptions(){
         return UserManager.getMessageOptionsList(AuthManager.getLoggedInUserID());
     }
 
+    /**
+     * Returns an array of the usernames based on type
+     * @param type type of the user
+     */
     public static String[] usersUsernames(String type) {
         ArrayList<String> users = UserTypeManager.getUsersByType(type);
         users.remove(UserManager.giveUsername(AuthManager.getLoggedInUserID()));
         return users.toArray(new String[0]);
     }
-
+    /**
+     * Returns the ID of the user using the given user's name
+     * @param name the name of the user
+     */
     public static int getUsersID(String name){
         return UserManager.giveIDOfUsername(name);
     }
 
+    /**
+     * Returns the senders username
+     * @param ID ID of the message
+     */
     public static String getSendersUsername(int ID){
         return UserManager.getUsernameFromID(MessageManager.getSendersID(ID));
     }
-
+    /**
+     * Displays the message
+     * @param message the message being displayed
+     */
     public static void createMessagePopUp(String message){
         mainView.createPopUp(message);
     }
